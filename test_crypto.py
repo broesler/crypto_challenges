58385127d07f69a4dc7946d09fc66365a6200c5d
Bernard Roesler <bernard.roesler@gmail.com>
2016-03-07 16:45:00 -0500

    Create gitignore for pyc files

--- test_crypto.py
+++ test_crypto.py
@@ -4,7 +4,7 @@
 #  Created: 03/07/2016, 13:42
 #   Author: Bernie Roesler
 #
-# Last Modified: 03/07/2016, 14:41
+# Last Modified: 03/07/2016, 16:43
 #
 '''
   Description: Test functions defined in crypto.py module
@@ -12,14 +12,15 @@
 #==============================================================================
 import crypto as crp
 
-def test(got, expected):
+def test(result, expected):
     ''' Compare expected result with actual result. '''
-    if got == expected:
+    if result == expected:
         prefix = "OK"
     else:
         prefix = " X"
 
-    print "%s : got: %s : expected: %s" % (prefix, repr(got), repr(expected))
+    print "%s : result: %s : expected: %s" \
+          % (prefix, repr(result), repr(expected))
     return
 
 #------------------------------------------------------------------------------
@@ -28,14 +29,28 @@ def test(got, expected):
 def main():
     ''' Call unit tests for functions in crypto.py '''
 
-    # Test hex2b64(s)
-    test(crp.hex2b64('Bernie'), 'Hi this is a string.')     # OK
-    test(crp.hex2b64('Bernie'), 'Not a string.')            #  X
+    #--------------------------------------------------------------------------
+    #       Test hex2b64 
+    #--------------------------------------------------------------------------
+    # Simple ASCII test
+    string = 'Man'
+    expect = 'TWFu'
+    test(crp.hex2b64(string.encode('hex')), expect)
+
+    # I/O test taken from <http://cryptopals.com/sets/1/challenges/1>
+    string = '49276d206b696c6c696e6720796f75722'\
+             '0627261696e206c696b65206120706f69'\
+             '736f6e6f7573206d757368726f6f6d'
+    expect = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsa'\
+             'WtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+    test(crp.hex2b64(string), expect)
+
+    # Run decode test
+    # test(crp.b642hex(string), expect)
 
     try:
         test(crp.hex2b64('BR'),     'Not a string.')        # ValueException
     except ValueError as errmsg:
-        # errmsg is a list, so take first item
         print 'Exception raised:'
         test(errmsg[0], 'Input argument must be at least 3 characters.')
 
