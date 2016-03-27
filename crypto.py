8205626fe7ca69895822fa7337da16c57790a052
Bernard Roesler <bernard.roesler@gmail.com>
2016-03-26 21:05:28 -0400

    [test_crypto.py] Remove error count from output.

--- crypto.py
+++ crypto.py
@@ -4,7 +4,7 @@
 #  Created: 03/03/2016, 14:55
 #   Author: Bernie Roesler
 #
-# Last Modified: 03/07/2016, 17:44
+# Last Modified: 03/26/2016, 21:04
 #
 '''
   Solutions to Matasano Crypto Challenges, Set 1.
@@ -79,15 +79,83 @@ def b64_int_dict(): #{{{
               63:'/'};
     return mydict #}}}
 
+def b64_chr_dict(): #{{{
+    ''' base64 dictionary with chars as keys. '''
+    mydict = {'A': 0,
+              'B': 1,
+              'C': 2,
+              'D': 3,
+              'E': 4,
+              'F': 5,
+              'G': 6,
+              'H': 7,
+              'I': 8,
+              'J': 9,
+              'K':10,
+              'L':11,
+              'M':12,
+              'N':13,
+              'O':14,
+              'P':15,
+              'Q':16,
+              'R':17,
+              'S':18,
+              'T':19,
+              'U':20,
+              'V':21,
+              'W':22,
+              'X':23,
+              'Y':24,
+              'Z':25,
+              'a':26,
+              'b':27,
+              'c':28,
+              'd':29,
+              'e':30,
+              'f':31,
+              'g':32,
+              'h':33,
+              'i':34,
+              'j':35,
+              'k':36,
+              'l':37,
+              'm':38,
+              'n':39,
+              'o':40,
+              'p':41,
+              'q':42,
+              'r':43,
+              's':44,
+              't':45,
+              'u':46,
+              'v':47,
+              'w':48,
+              'x':49,
+              'y':50,
+              'z':51,
+              '0':52,
+              '1':53,
+              '2':54,
+              '3':55,
+              '4':56,
+              '5':57,
+              '6':58,
+              '7':59,
+              '8':60,
+              '9':61,
+              '+':62,
+              '/':63};
+    return mydict #}}}
+
 #------------------------------------------------------------------------------
 #       Convert hexadecimal string to base64 string 
 #------------------------------------------------------------------------------
-def hex2b64(str_type):
-    ''' Convert string from hexadecimal to base64. '''
+def hex2b64_str(str_type):
+    ''' Convert hex string to base64 string. '''
     # lookup table of base64 characters, keys are integers
     b64_lut = b64_int_dict()    
 
-    # # Need multiples of 3 bytes to get base64 (6 bits per char)
+    # Need multiples of 3 bytes to get base64 (6 bits per char)
     if len(str_type) < 3:
         raise ValueError('Input argument must be at least 3 characters.')
 
@@ -111,16 +179,33 @@ def hex2b64(str_type):
 
     return b64_str
 
+# def hex2b64_int(int_type):
+#     ''' Convert hex int to base64 int. '''
+#     # lookup table of base64 characters, keys are chars
+#     b64_lut = b64_chr_dict()    
+#
+#     nchr = 2*len(str(int_type)) / 3
+#
+#     return b64_int
+
+# def b64_encode(str_type)
+#     ''' Encode base64 string as integer '''
+#     return b64_int
+
 #------------------------------------------------------------------------------
 #       Fixed-length XOR 
 #------------------------------------------------------------------------------
 def fixedXOR(str1, key):
+    ''' Take a hex-encoded string and XOR it with a hex-encoded key to return
+    a hex-encoded string. '''
+
     if len(str1) != len(key):
         raise ValueError('Input strings must be of equal length!')
 
     # XOR the numbers
-    out = int(str1,16) ^ int(key,16)
-    return out
+    out_int = int(str1,16) ^ int(key,16)
+
+    return out_int
 
 #==============================================================================
 #==============================================================================
--- test_crypto.py
+++ test_crypto.py
@@ -4,7 +4,7 @@
 #  Created: 03/07/2016, 13:42
 #   Author: Bernie Roesler
 #
-# Last Modified: 03/07/2016, 17:43
+# Last Modified: 03/26/2016, 20:59
 #
 '''
   Description: Test functions defined in crypto.py module
@@ -30,31 +30,36 @@ def main():
     ''' Call unit tests for functions in crypto.py '''
 
     #--------------------------------------------------------------------------
-    #       Test hex2b64 
+    #       Test hex2b64_str
     #--------------------------------------------------------------------------
-    print '---- hex2b64 ----'
-    # Simple ASCII test
+    print '---- hex2b64_str ----'
+    # ASCII string to base64 string
     string = 'Man'
     expect = 'TWFu'
-    test(crp.hex2b64(string.encode('hex')), expect)
 
+    # Convert ASCII string to hex before passing
+    test(crp.hex2b64_str(string.encode('hex')), expect)
+
+    # Hex string to base64 string
     # I/O test taken from <http://cryptopals.com/sets/1/challenges/1>
     string = '49276d206b696c6c696e6720796f75722'\
              '0627261696e206c696b65206120706f69'\
              '736f6e6f7573206d757368726f6f6d'
     expect = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsa'\
              'WtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-    test(crp.hex2b64(string), expect)
 
-    # Test for a string that is too short 
+    # Input already a hex-encoded string
+    test(crp.hex2b64_str(string), expect)
+
+    # Test for a string that is too short
     try:
-        test(crp.hex2b64('B'.encode('hex')), 'Whatever.')        
+        test(crp.hex2b64_str('B'), 'Whatever.')
     except ValueError as errmsg:
         test(errmsg[0], 'Input argument must be at least 3 characters.')
 
     # Test for output padding (or lack thereof)
     try:
-        test(crp.hex2b64('sure.'.encode('hex')), 'c3VyZS4=')        
+        test(crp.hex2b64_str('sure.'.encode('hex')), 'c3VyZS4=')
     except Exception as errmsg:
         test(errmsg[0], 'Number of input bytes not divisible by 6, not'\
                 ' including padding characters in output.')
@@ -63,20 +68,29 @@ def main():
     #        Test fixedXOR
     #--------------------------------------------------------------------------
     print '---- fixedXOR ----'
-    str1    = '1c0111001f010100061a024b53535009181c'
-    key     = '686974207468652062756c6c277320657965'
-    expect  = '746865206b696420646f6e277420706c6179' 
+    # Test for short string
+    str1   = 'A'
+    key    = '2'
+    expect = '8'
 
-    # Return an integer XOR
-    test_int = crp.fixedXOR(str1,key) 
-
+    out_int = crp.fixedXOR(str1,key)
     # Python hex() inclues '0x...L' for hex long integers, so remove them
-    test_str = hex(test_int).rstrip('L').lstrip('0x') or '0'
-    test(test_str, expect)
+    out_str = hex(out_int).lstrip('0x').rstrip('L') or '0'
+
+    test(out_str, expect)
+
+    # I/O test from <http://cryptopals.com/sets/1/challenges/2>
+    str1   = '1c0111001f010100061a024b53535009181c'
+    key    = '686974207468652062756c6c277320657965'
+    expect = '746865206b696420646f6e277420706c6179'
+
+    out_int = crp.fixedXOR(str1,key)
+    out_str = hex(out_int).lstrip('0x').rstrip('L') or '0'
+    test(out_str, expect)
 
     # Test for different length inputs
     try:
-        test( crp.fixedXOR('abc','de'), 'error?')
+        test( hex(crp.fixedXOR('abc','de')).lstrip('0x').rstrip('L'), 'error?')
     except ValueError as errmsg:
         test(errmsg[0], 'Input strings must be of equal length!')
 
