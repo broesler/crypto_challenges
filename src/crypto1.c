e6e3cff2af6b48222abeca3cb84654d62b306382
Bernard Roesler <bernard.roesler@gmail.com>
2017-07-20 13:41:05 -0400

    Add AES encrypt/decrypt functions.

--- src/crypto1.c
+++ src/crypto1.c
@@ -501,5 +501,93 @@ XOR_NODE *breakRepeatingXOR(const char *byte, size_t nbyte)
     return out;
 }
 
+/*------------------------------------------------------------------------------
+ *         Error handling for OpenSSL libs
+ *----------------------------------------------------------------------------*/
+void handleErrors(void)
+{
+    /* Just dump to stderr */
+    ERR_print_errors_fp(stderr);
+    ERROR("AES encountered an error.");
+}
+
+/*------------------------------------------------------------------------------
+ *         Encryption function 
+ *----------------------------------------------------------------------------*/
+int aes_128_ecb_encrypt(unsigned char *plaintext, int plaintext_len,
+        unsigned char *key, unsigned char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+    int ciphertext_len;
+
+    /* Create and initialise the context */
+    if (!(ctx = EVP_CIPHER_CTX_new())) { handleErrors(); }
+
+    /* Initialise the encryption operation using AES 128-bit ECB */
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, key, NULL)) {
+        handleErrors();
+    }
+
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary */
+    if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) {
+        handleErrors();
+    }
+    ciphertext_len = len;
+
+    /* Finalise the encryption. Further ciphertext bytes may be written. */
+    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) { 
+        handleErrors(); 
+    }
+    ciphertext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
+
+/*------------------------------------------------------------------------------
+ *         Decryption function 
+ *----------------------------------------------------------------------------*/
+int aes_128_ecb_decrypt(unsigned char *ciphertext, int ciphertext_len, 
+        unsigned char *key, unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+    int plaintext_len;
+
+    /* Create and initialise the context */
+    if (!(ctx = EVP_CIPHER_CTX_new())) { handleErrors(); }
+
+    /* Initialise the decryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits */
+    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, NULL)) { 
+        handleErrors(); 
+    }
+
+    /* Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary */
+    if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) {
+        handleErrors(); 
+    }
+    plaintext_len = len;
+
+    /* Finalise the decryption. Further plaintext bytes may be written. */
+    if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) { handleErrors(); }
+    plaintext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return plaintext_len;
+}
+
 /*==============================================================================
  *============================================================================*/
