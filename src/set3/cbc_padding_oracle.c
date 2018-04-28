d9219ccecaef1ce576a8e9ce62fb5d01ac82078d
Bernard Roesler <bernard.roesler@gmail.com>
2018-04-27 21:33:35 -0400

    .gitignore in makefile. Begin padding oracle.

--- src/set3/cbc_padding_oracle.c
+++ src/set3/cbc_padding_oracle.c
@@ -8,6 +8,7 @@
  *============================================================================*/
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "header.h"
 #include "aes_openssl.h"
@@ -22,7 +23,7 @@ static BYTE *global_key = NULL;
 static BYTE *global_iv  = NULL;
 
 /* String to be encrypted */
-static const char * const possible_x[10] = 
+static const char * const POSSIBLE_X[10] = 
 { 
     "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=", 
     "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=", 
@@ -39,28 +40,47 @@ static const char * const possible_x[10] =
 /* Encrypt randomly one of the above strings, return ciphertext and set IV */
 int encryption_oracle(BYTE **y, size_t *y_len);
 
+/* Decrypt ciphertext and return 0 for valid padding or -1 for invalid */
+int decrypt_and_checkpad(BYTE *y, size_t y_len);
+
 /*------------------------------------------------------------------------------
  *         Main function
  *----------------------------------------------------------------------------*/
 int main(int argc, char **argv)
 {
-    /* initialize PRNG */
-    srand(SRAND_INIT);
-
-    /* Encrypt random string */
     BYTE *y = NULL;
     size_t y_len = 0;
-    if (0 != encryption_oracle(&y, &y_len)) {
-        ERROR("Incorrect padding!");
-    }
 
-    /* Decrypt it and report if padding is valid or not */
-    BYTE *x = NULL;
-    size_t x_len = 0;
-    /* Test == -1 for invalid padding */
-    int test = aes_128_cbc_decrypt(&x, &x_len, y, y_len, global_key, global_iv);
+    /* initialize PRNG */
+    srand(SRAND_INIT); /* init for deterministic result */
+    /* srand(time(NULL)); */
 
-    printf("x = %s\ntest = %d\n", x, test);
+    /* Encrypt random string */
+    encryption_oracle(&y, &y_len);
+
+    /* Create copy of ciphertext */
+    BYTE *yp = init_byte(y_len);
+    memcpy(yp, y, y_len);
+    /* Let last byte yp be 0x01 */
+    BYTE pad_byte = 0x01;
+    yp[y_len-1] = pad_byte;
+
+    BYTE *pad_arr = init_byte(y_len);
+    pad_arr[y_len-1] = pad_byte;
+
+    int test = decrypt_and_checkpad(yp, y_len);
+
+    if (!test) {
+       /* Last byte of x is yp ^ pad_byte */
+        BYTE *x_blocks = fixedXOR(yp, pad_arr, y_len);
+    } /* else { */
+    /* } */
+
+#ifdef LOGSTATUS
+    /* test == -1 for invalid padding */
+    char *status = (test == 0) ? "valid" : "invalid";
+    printf("Padding %s!\n", status);
+#endif
 
     free(y);
     free(global_key);
@@ -73,8 +93,11 @@ int main(int argc, char **argv)
  *----------------------------------------------------------------------------*/
 int encryption_oracle(BYTE **y, size_t *y_len)
 {
-    int out = 0,
-        choice = 0;
+    /* Randomly select one of possible inputs */
+    int choice = RAND_RANGE(0, 9);
+    const char *x = POSSIBLE_X[choice];
+    size_t x_len = strlen(x);
+
     *y_len = 0;
 
     /* Generate a random key ONCE */
@@ -87,26 +110,28 @@ int encryption_oracle(BYTE **y, size_t *y_len)
         global_iv = rand_byte(BLOCK_SIZE);
     }
 
-    /* Randomly select one of possible inputs */
-    choice = RAND_RANGE(0, 9);
-    const char *x = possible_x[choice];
-    size_t x_len = strlen(x);
-
 #ifdef LOGSTATUS
-    printf("Chose string %d:\n    %s\n", choice, x);
+    printf("Chose string %d: %s\n", choice, x);
 #endif
 
     /* Encrypt using CBC mode */
-    out = aes_128_cbc_encrypt(y, y_len, (BYTE *)x, x_len, global_key, global_iv);
-
-    /* Padding is invalid */
-    if (0 != out) { 
-        return -1; 
-    }
-
+    aes_128_cbc_encrypt(y, y_len, (BYTE *)x, x_len, global_key, global_iv);
     return 0;
 }
 
+
+/*------------------------------------------------------------------------------
+ *          Decrypt and Check Padding
+ *----------------------------------------------------------------------------*/
+int decrypt_and_checkpad(BYTE *y, size_t y_len)
+{
+    /* Decrypt y report if padding is valid or not, but do not return x */
+    BYTE *x = NULL;
+    size_t x_len = 0;
+    int test = aes_128_cbc_decrypt(&x, &x_len, y, y_len, global_key, global_iv);
+    free(x);
+    return test;
+}
 
 /*==============================================================================
  *============================================================================*/
