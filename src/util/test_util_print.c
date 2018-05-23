f3e5bbc5b07ad3adc352c0c1c4f6b3e2e10c83d9
Bernard Roesler <bernard.roesler@gmail.com>
2018-05-23 16:47:41 -0400

    Clean up dup printing code.

--- src/util/test_util_print.c
+++ src/util/test_util_print.c
@@ -3,7 +3,7 @@
  *  Created: 05/07/2018, 17:20
  *   Author: Bernie Roesler
  *
- *  Description: 
+ *  Description:
  *
  *============================================================================*/
 
@@ -25,13 +25,13 @@ int IsPrintable1()
     char str1[] = "Anything less than the best is a felony.";
     int test = isprintable((BYTE *)str1, strlen(str1));
     int expect = 1;
-    SHOULD_BE(test == expect); 
+    SHOULD_BE(test == expect);
 #ifdef LOGSTATUS
     printf("Got:    %d\nExpect: %d\n", test, expect);
 #endif
     END_TEST_CASE;
 }
-    
+
 int IsPrintable2()
 {
     START_TEST_CASE;
@@ -39,12 +39,13 @@ int IsPrintable2()
     char str2[] = "Anything \u1801less than the best is a felony.";
     int test = isprintable((BYTE *)str2, strlen(str2));
     int expect = 0;
-    SHOULD_BE(test == expect); 
+    SHOULD_BE(test == expect);
 #ifdef LOGSTATUS
     printf("Got:    %d\nExpect: %d\n", test, expect);
 #endif
     END_TEST_CASE;
 }
+
 
 /* This function tests the printall function for proper hex codes*/
 int PrintAll1()
@@ -53,27 +54,27 @@ int PrintAll1()
     char str1[] = "Anything less than the best is a felony.";
     size_t len1 = strlen(str1);
     char *buffer = init_str(len1);  /* should be exactly same length */
-    /* if ((fd = open("/dev/null", O_WRONLY)) < 0) { */
-    FILE *fp = NULL;
-    if (!(fp = fopen("/dev/null", "a"))) {
+    int fd = -1,
+        saveout = -1;
+    if ((fd = open("/dev/null", O_WRONLY)) < 0) {
         ERROR("Failed to open file!");
     }
-    int fd = fileno(fp);
-    int saveout;
-    if ((saveout = dup(STDOUT_FILENO)) < 0) { ERROR("dup failed."); }
-    fflush(stdout);
-    /* redirect stdout */
-    if (dup2(fd, STDOUT_FILENO) < 0) { ERROR("dup2 failed."); }
+    if ((saveout = dup(STDOUT_FILENO)) < 0) {
+        ERROR("dup failed.");
+    }
+    fflush(stdout); /* flush stdout before redirecting */
+    if (dup2(fd, STDOUT_FILENO) < 0) {  /* redirect stdout */
+        ERROR("dup2 failed.");
+    }
     close(fd);
-    setbuffer(stdout, buffer, len1); /* buffer stdout to our own buffer */
+    setvbuf(stdout, buffer, _IOFBF, len1); /* buffer stdout to our own buffer */
     /*----- RUN TEST -----*/
     printall((BYTE *)str1, len1);
-    fflush(stdout);
-    SHOULD_BE(!strncmp(str1, buffer, len1)); 
-    /* Redirect stdout back to original location for normal output */
-    dup2(saveout, 1);
+    SHOULD_BE(!strncmp(str1, buffer, len1));
+    /*----- END TEST -----*/
+    setvbuf(stdout, NULL, _IOLBF, 0); /* reset buffering of stdout */
+    dup2(saveout, 1);                 /* redirect stdout back to original */
     close(saveout);
-    fclose(fp);
 #ifdef LOGSTATUS
     printf("Got:    %s\nExpect: %s\n", buffer, str1);
 #endif
@@ -95,7 +96,7 @@ int main(void)
 
     /* Count errors */
     if (!fails) {
-        printf("\033[0;32mAll %d tests passed!\033[0m\n", total); 
+        printf("\033[0;32mAll %d tests passed!\033[0m\n", total);
         return 0;
     } else {
         printf("\033[0;31m%d/%d tests failed!\033[0m\n", fails, total);
