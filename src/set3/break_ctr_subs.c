82d02e0671e67d31d44d9a7b8447c5d90635de31
Bernard Roesler <bernard.roesler@gmail.com>
2018-10-12 00:31:23 -0400

    Move file reading to function in break_ctr_subs.c

--- src/set3/break_ctr_subs.c
+++ src/set3/break_ctr_subs.c
@@ -16,6 +16,10 @@
 #include "crypto2.h"
 #include "crypto3.h"
 
+BYTE *key = (BYTE *)"YELLOW SUBMARINE";
+
+size_t read_and_encrypt_file(char *b64_file, BYTE **y_lines, int *y_nums);
+
 int main(int argc, char **argv)
 {
     char *b64_file = NULL;
@@ -27,6 +31,47 @@ int main(int argc, char **argv)
         b64_file = argv[1];
     }
 
+    /* Allocate array to store encrypted byte arrays */
+    size_t Nl = lines_in_file(b64_file);
+    BYTE **y_lines = calloc(Nl, sizeof(BYTE *));
+    MALLOC_CHECK(y_lines);
+    int *y_nums = init_int(Nl);  /* array of nbytes per line */
+
+    if (read_and_encrypt_file(b64_file, y_lines, y_nums) != Nl) {
+        ERROR("Not all lines read from file!");
+    }
+
+    /* Print encrypted bytes */
+#ifdef LOGSTATUS
+    LOG("y_lines:");
+    for (size_t i = 0; i < Nl; i++) {
+        printf("[%2lu]: \"", i);
+        print_blocks(*(y_lines+i), *(y_nums+i), BLOCK_SIZE, 0);
+        printf("\"\n");
+    }
+#endif
+
+    /* Decrypt results */
+
+    free_str_arr((char **)y_lines, Nl);
+    free(y_nums);
+    return 0;
+}
+
+/*------------------------------------------------------------------------------
+ *          Function definitions        
+ *----------------------------------------------------------------------------*/
+size_t read_and_encrypt_file(char *b64_file, BYTE **y_lines, int *y_nums)
+{
+    size_t n_lines = 0;
+
+    /* Fixed key and nonce */
+    BYTE *nonce = init_byte(BLOCK_SIZE/2);
+
+
+#ifdef LOGSTATUS
+    LOG("Reading from file '%s'...", b64_file);
+#endif
     /* For each line in file:
      * - Read base64 line
      * - Convert to byte array
@@ -37,44 +82,27 @@ int main(int argc, char **argv)
     if (!fp) {
         ERROR("File %s could not be read!", b64_file);
     }
-    
-    /* Allocate array to store encrypted byte arrays */
-    size_t Nl = lines_in_file(b64_file);
-    BYTE **y_lines = calloc(Nl, sizeof(BYTE *));
-    MALLOC_CHECK(y_lines);
-    int *y_nums = init_int(Nl);     /* array of nbytes per line */
 
-    /* Fixed key and nonce */
-    BYTE *key = (BYTE *)"YELLOW SUBMARINE";
-    BYTE *nonce = init_byte(BLOCK_SIZE/2);
-
-    /* Pointers to arrays */
-    BYTE **yl = y_lines;
+    char *line = init_str(MAX_LINE_LEN);  /* buffer for each file line */
+    BYTE **yl = y_lines;         /* temp pointers to arrays */
     int *yn = y_nums;
 
-    /* Buffer for each file line */
-    char *line = init_str(MAX_LINE_LEN);
-
-#ifdef LOGSTATUS
-        LOG("Reading from file '%s'...", b64_file);
-#endif
-
     while (fgets(line, MAX_LINE_LEN, fp)) {
+        n_lines++;
         char *b64_clean = strrmchr(line, "\n");  /* strip newlines */
 
         /* Convert to byte array for encryption */
         BYTE *byte = NULL;
         size_t nbyte = b642byte(&byte, b64_clean);
 
+        /* Encrypt using CTR with fixed nonce and key */
         FILE *xs = fmemopen(byte, nbyte, "r");
         FILE *ys = tmpfile();
-
-        /* Encrypt using CTR with fixed nonce and key */
         if (aes_128_ctr(ys, xs, key, nonce)) {
             ERROR("Encryption failed!\n    line = '%s'", b64_clean);
         }
 
-        /* Store in array */
+        /* Store encrypted bytes in array */
         BYTE *yb = init_byte(nbyte);
         if (!fread(yb, 1, nbyte, ys)) {
             ERROR("File read error!\n    line = '%s'", b64_clean);
@@ -87,28 +115,14 @@ int main(int argc, char **argv)
         free(b64_clean);
         fclose(xs);
         fclose(ys);
-    }
+    } /* end read from file */
 
-/*     #<{(| Print encrypted bytes |)}># */
-/* #ifdef LOGSTATUS */
-/*     LOG("y_lines:"); */
-/*     for (size_t i = 0; i < Nl; i++) { */
-/*         printf("[%2lu]: \"", i); */
-/*         print_blocks(*(y_lines+i), *(y_nums+i), BLOCK_SIZE, 0); */
-/*         printf("\"\n"); */
-/*     } */
-/* #endif */
-
-    /* Decrypt results */
-
-    free_str_arr((char **)y_lines, Nl);
     free(line);
     free(nonce);
-    free(y_nums);
     fclose(fp);
-    return 0;
-}
 
+    return n_lines;
+}
 
 /*==============================================================================
  *============================================================================*/
